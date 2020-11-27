239aac2baea57a991374a1e49eebcc013823c317
Mario Baroni <mariobaroni@outlook.com>
2020-11-27 19:59:07 +0100

    fix: decorators

--- packages/core/src/decorators/index.ts
+++ packages/core/src/decorators/index.ts
@@ -3,3 +3,4 @@ export * from './api-controller';
 export * from './parse-string';
 export * from './api-query';
 export * from './ref-type';
+export * from './property-decorators';
--- packages/core/src/model/index.ts
+++ packages/core/src/model/index.ts
@@ -3,6 +3,5 @@ export * from './base.model';
 export * from './crud.service';
 export * from './crud.controller';
 export * from './http-error';
-export * from './property-decorators';
 export * from './dto';
 export * from './validation';
